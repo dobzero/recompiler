17888450de9f0be57ac22edf237996d3ea64b541
callsvc <gabriel_correia0202@outlook.com>
2026-08-14 20:33:53 -0300

    (recompiler), code cleanup at Layer::ctrl_modify_archs

--- layer.cpp
+++ layer.cpp
@@ -9,31 +9,31 @@ Layer::Layer() {
 
 // modify how will be the guest and host processor
 void Layer::ctrl_modify_archs(const SupportedFrontends guest_type, const SupportedBackends backend_type) {
-    if (from_cpu_g && from_cpu_g->type!=guest_type)
-        from_cpu_g.reset();
-    if (target_cpu_h && target_cpu_h->type!=backend_type)
-        from_cpu_g.reset();
-
-    if (!from_cpu_g) {
-        switch (guest_type) {
-            case SupportedFrontends::Frontend_ARM_V8_A:
-                from_cpu_g=std::make_unique<fe::v8_a::ProcessorArm64v8a>();
-                break;
-            default:
-                return;
-        }
-        // checking compatibility of both mechanism
-        if (auto &supported_archs_type = from_cpu_g->supported_target_cpu_type; supported_archs_type[0]!=backend_type||supported_archs_type[1]!=backend_type) {
-
-        }
+    if (!from_cpu_g || from_cpu_g->type!=guest_type) {
+        from_cpu_g=[&guest_type]() -> std::unique_ptr<fe::FrontendProcessorCaps> {
+            switch (guest_type) {
+                case SupportedFrontends::Frontend_ARM_V8_A:
+                    return std::make_unique<fe::v8_a::ProcessorArm64v8a>();
+                    break;
+                default:
+                    return nullptr;
+            }
+        }();
     }
-    if (!target_cpu_h) {
-        switch (backend_type) {
-            case SupportedBackends::Arch_ARM_64:
-                return;
-            case SupportedBackends::Arch_X_86_64:
-                target_cpu_h=std::make_unique<be::x86_64::X86InplaceAsm>();
-        }
+    if (!target_cpu_h || target_cpu_h->type!=backend_type) {
+        target_cpu_h=[&backend_type]() -> std::unique_ptr<be::BackendProcessorCaps> {
+            switch (backend_type) {
+                case SupportedBackends::Arch_X_86_64:
+                    return std::make_unique<be::x86_64::X86InplaceAsm>();
+                case SupportedBackends::Arch_ARM_64:
+                default:
+                    return nullptr;
+            }
+        }();
+    }
+
+    // checking compatibility of both mechanism
+    if (auto &supported_archs_type = from_cpu_g->supported_target_cpu_type; supported_archs_type[0]!=backend_type||supported_archs_type[1]!=backend_type) {
     }
 }
 
